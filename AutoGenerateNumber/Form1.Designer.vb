6a76fc30cf34704003d08c746c1717c1ee5a8d63
Najwa <najwa@iris.com.my>
2022-09-19 08:49:00 +0800

    first commit

---
+++ AutoGenerateNumber/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.AutoNumbers = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.MenuHighlight
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(1, 1)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(328, 29)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 98)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(306, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Generate Numbers"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'AutoNumbers
+        '
+        Me.AutoNumbers.Location = New System.Drawing.Point(12, 49)
+        Me.AutoNumbers.Multiline = True
+        Me.AutoNumbers.Name = "AutoNumbers"
+        Me.AutoNumbers.Size = New System.Drawing.Size(306, 43)
+        Me.AutoNumbers.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(81, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(164, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "AUTO GENERATED NUMBERS"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(330, 129)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.AutoNumbers)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents AutoNumbers As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ AutoGenerateNumber/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Dim x As String = ""
+    Dim rand As Random = New Random()
+
+    Sub generate()
+        For y As Integer = 1 To 7
+            x = Date.Now.Year
+            If y = 7 Then
+                x += "000"
+            End If
+        Next
+        For y As Integer = 1 To 6
+            x += Convert.ToString(rand.Next(0, 9))
+        Next
+
+        AutoNumbers.Text = x
+        If AutoNumbers.Text = x Then
+            x = Nothing
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        generate()
+    End Sub
+End Class
